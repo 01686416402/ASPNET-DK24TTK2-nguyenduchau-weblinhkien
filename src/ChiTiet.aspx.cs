6944cfed25f07a765f7bfea60af0c1a264bee9f2
Duc Hau <46512788+01686416402@users.noreply.github.com>
2026-05-02 16:19:36 +0700

    Add files via upload

---
+++ src/ChiTiet.aspx.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Globalization;
+
+namespace WebDientu
+{
+    public partial class ChiTiet : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+                LoadSP();
+        }
+
+        // ===== LOAD SẢN PHẨM =====
+        void LoadSP()
+        {
+            int id;
+            if (!int.TryParse(Request.QueryString["id"], out id)) return;
+
+            string connStr = ConfigurationManager.ConnectionStrings["conn"].ConnectionString;
+
+            using (SqlConnection conn = new SqlConnection(connStr))
+            {
+                SqlCommand cmd = new SqlCommand("SELECT * FROM SanPham WHERE MaSP=@id", conn);
+                cmd.Parameters.AddWithValue("@id", id);
+
+                conn.Open();
+                SqlDataReader r = cmd.ExecuteReader();
+
+                if (r.Read())
+                {
+                    lblTen.Text = r["TenSP"].ToString();
+
+                    int gia = Convert.ToInt32(r["Gia"]);
+                    lblGia.Text = String.Format(new CultureInfo("vi-VN"), "{0:N0}", gia);
+
+                    lblLoai.Text = r["Loai"].ToString();
+                    lblThuocTinh.Text = r["ThuocTinh"].ToString();
+
+                    imgSP.ImageUrl = "image/" + r["HinhAnh"].ToString();
+                }
+
+                r.Close();
+            }
+        }
+    }
+}
---
+++ src/Default.aspx.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Data;
+using System.Web.UI;
+
+namespace WebDientu
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                LoadLoai();
+                LoadSanPham();
+            }
+        }
+
+        // LOAD LOẠI
+        void LoadLoai()
+        {
+            ketnoi kn = new ketnoi();
+            DataTable dt = kn.getData("SELECT DISTINCT Loai FROM SanPham");
+
+            rpLoai.DataSource = dt;
+            rpLoai.DataBind();
+        }
+
+        // LOAD SẢN PHẨM
+        void LoadSanPham()
+        {
+            ketnoi kn = new ketnoi();
+
+            string loai = Request.QueryString["loai"];
+            string keyword = Request.QueryString["search"];
+
+            string sql = "SELECT * FROM SanPham WHERE 1=1";
+
+            if (!string.IsNullOrEmpty(loai))
+                sql += " AND Loai = N'" + loai + "'";
+
+            if (!string.IsNullOrEmpty(keyword))
+                sql += " AND TenSP LIKE N'%" + keyword + "%'";
+
+            DataTable dt = kn.getData(sql);
+
+            rpSP.DataSource = dt;
+            rpSP.DataBind();
+        }
+
+        // SEARCH
+        protected void btnSearch_Click(object sender, EventArgs e)
+        {
+            string keyword = txtSearch.Text.Trim();
+            string loai = Request.QueryString["loai"];
+
+            string url = "Default.aspx";
+
+            if (!string.IsNullOrEmpty(keyword) || !string.IsNullOrEmpty(loai))
+            {
+                url += "?";
+
+                if (!string.IsNullOrEmpty(keyword))
+                    url += "search=" + keyword;
+
+                if (!string.IsNullOrEmpty(loai))
+                {
+                    if (!string.IsNullOrEmpty(keyword)) url += "&";
+                    url += "loai=" + loai;
+                }
+            }
+
+            Response.Redirect(url);
+        }
+    }
+}
